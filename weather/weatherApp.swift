d86137bb41f4cb08e37429dddd054cc27c437736
Amir Abaris <thisisamirabaris@gmail.com>
2025-05-30 17:54:36 +0330

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Amir Abaris on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
